d00fc3f5bf5f1c31ecd2095bd3223f2c570e4ade
Chris.Franklin <cfrank306@gmail.com>
2021-11-03 14:37:41 -0500

    fixed models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.hasOne(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  foreignKey: 'product_tag',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  foreignKey: 'product_tag',
+});
 
 module.exports = {
   Product,
